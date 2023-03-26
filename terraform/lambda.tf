21d286927f9bd795d97d1e565b4ce0f9b0f66926
Aleksandr_Filichkin <Aleksandr.Filichkin@refinitiv.com>
2023-03-26 15:15:18 +0200

    without snap-start

--- terraform/lambda.tf
+++ terraform/lambda.tf
@@ -15,7 +15,7 @@ resource "aws_lambda_function" "example_lambda" {
   handler          = "com.filichkin.blog.lambda.v2.handler.BookHandler::handleRequest"
   filename         = local.lambda_payload_filename
   role             = aws_iam_role.lambda_role.arn
-  memory_size      = 1536
+  memory_size      = 10240
   timeout          = 30
 #  snap_start {
 #    apply_on = "PublishedVersions"
